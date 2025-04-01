61d3602835ac56e0b10feab55483323be419d98f
Katheringl <kgaleanol@estudiante.uniajc.edu.co>
2025-04-01 17:05:45 +0000

    Se agregó el metodo elementAt para recorrer la pila de trabajadores y poder mostrar sus nombres

--- Clase9/Trabajador.java
+++ Clase9/Trabajador.java
@@ -1,14 +1,37 @@
 package Clase9;
-
 public class Trabajador {
-    // Atributos de la clase
+
     private String cedula;
     private String nombre;
     private double salario;
-
-    // Constructor de la clase
-    public Trabajador(String cedula, String nombre, double salario) {
+    
+    public Trabajador(String cedula, String nombre, double salario){
         this.cedula = cedula;
         this.nombre = nombre;
         this.salario = salario;
     }
+
+    public String getCedula(){
+        return cedula;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public double getSalario(){
+        return salario;
+    }
+
+    public String toString(){
+        return "Trabajador { Cedula: " + cedula + " Nombre: " + nombre + " Salario: " + salario + "}"; 
+    }
+
+    public double pagar(double incremento){
+        return salario + (salario * incremento);
+    }
+
+}
+
+    
+
